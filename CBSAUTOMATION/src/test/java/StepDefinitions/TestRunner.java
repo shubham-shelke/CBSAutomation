c3320818ee4d95379e6e49d56c3d9e8d689868d1
Shubham Balasaheb Shelke <97661425+shubham-shelke@users.noreply.github.com>
2025-06-26 18:55:43 +0530

    Update TestRunner.java

--- CBSAUTOMATION/src/test/java/StepDefinitions/TestRunner.java
+++ CBSAUTOMATION/src/test/java/StepDefinitions/TestRunner.java
@@ -2,9 +2,10 @@ package StepDefinitions;
 
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = {
