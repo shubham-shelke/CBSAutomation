c39bfbd4ade0d1602270f04b2594a151695502a4
Shubham Balasaheb Shelke <97661425+shubham-shelke@users.noreply.github.com>
2025-07-03 09:38:35 +0530

    Update RunCucumberTestNG.java

--- CBSAUTOMATION/src/test/java/StepDefinitions/RunCucumberTestNG.java
+++ CBSAUTOMATION/src/test/java/StepDefinitions/RunCucumberTestNG.java
@@ -1,3 +1,43 @@
+// package StepDefinitions;
+
+// import org.junit.runner.RunWith;
+// import io.cucumber.junit.Cucumber;
+
+// import io.cucumber.testng.AbstractTestNGCucumberTests;
+// import io.cucumber.testng.CucumberOptions;
+
+// @RunWith(Cucumber.class)
+// @CucumberOptions(
+//     features = {
+//     		"src\\test\\resources\\Features"
+//            //  "C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\CBSAutomation\\CBSAUTOMATION\\src\\test\\resources\\Features"
+//              //"https://github.com/shubham-shelke/CBSAutomation/tree/main/CBSAUTOMATION/src/test/resources/Features",
+// //    		"D:\\CBS Automation\\CBSCucumber\\CBSAUTOMATION\\src\\test\\resources\\Featurepreprod",
+//     },
+//     glue = {"StepDefinitions"},
+//     plugin = {
+//        "pretty",
+//         "junit:target/JunitReports/reports.xml",
+//        //  "json:target/JsonReports/reports.json",
+//        // "html:target/HtmlReports.html"
+//          "html:target/cucumber-reports/report.html",
+//         // "json:target/cucumber-reports/reports.json",
+//        "json:target/cucumber-reports/report.json"
+//    },
+//     monochrome = true ,
+// //    tags = "@Login or @SurveyDetails or @Pricing or @Approver "
+//      tags = "@Login or @Enquiry or @SurveyDetails or @Pricing or @Approver or @MoveMan or @QuoteApprover or @Billing"
+// //    tags = "@Login or @SurveyDetails or @Pricing or @Approver or @MoveMan or @QuoteApprover or @Billing"
+// //    tags = "@Login or @Pricing or @Approver or @Login or @MoveMan or @QuoteApprover or @Billing"
+// //    tags ="@Approver or @Login or @MoveMan or @QuoteApprover"
+// //    tags = "@Login or @MoveMan or @QuoteApprover or @Billing "
+// //    tags = "@QuoteApprover or @Billing"
+// //    tags = "@Billing"
+// )
+// public class RunCucumberTestNG extends AbstractTestNGCucumberTests{
+// }
+
+
 package StepDefinitions;
 
 import org.junit.runner.RunWith;
@@ -6,48 +46,37 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+import java.io.File;                       // NEW
+import org.testng.annotations.BeforeClass; // NEW
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = {
-    		"src\\test\\resources\\Features"
-           //  "C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\CBSAutomation\\CBSAUTOMATION\\src\\test\\resources\\Features"
-             //"https://github.com/shubham-shelke/CBSAutomation/tree/main/CBSAUTOMATION/src/test/resources/Features",
-//    		"D:\\CBS Automation\\CBSCucumber\\CBSAUTOMATION\\src\\test\\resources\\Featurepreprod",
+        "src\\test\\resources\\Features"
     },
     glue = {"StepDefinitions"},
     plugin = {
-       "pretty",
+        "pretty",
         "junit:target/JunitReports/reports.xml",
-       //  "json:target/JsonReports/reports.json",
-       // "html:target/HtmlReports.html"
-         "html:target/cucumber-reports/report.html",
-        // "json:target/cucumber-reports/reports.json",
-       "json:target/cucumber-reports/report.json"
-   },
-    monochrome = true ,
-//    tags = "@Login or @SurveyDetails or @Pricing or @Approver "
-     tags = "@Login or @Enquiry or @SurveyDetails or @Pricing or @Approver or @MoveMan or @QuoteApprover or @Billing"
-//    tags = "@Login or @SurveyDetails or @Pricing or @Approver or @MoveMan or @QuoteApprover or @Billing"
-//    tags = "@Login or @Pricing or @Approver or @Login or @MoveMan or @QuoteApprover or @Billing"
-//    tags ="@Approver or @Login or @MoveMan or @QuoteApprover"
-//    tags = "@Login or @MoveMan or @QuoteApprover or @Billing "
-//    tags = "@QuoteApprover or @Billing"
-//    tags = "@Billing"
+        "html:target/cucumber-reports/report.html",
+        "json:target/cucumber-reports/report.json"
+    },
+    monochrome = true,
+    tags = "@Login or @Enquiry or @SurveyDetails or @Pricing or @Approver or @MoveMan or @QuoteApprover or @Billing"
 )
-public class RunCucumberTestNG extends AbstractTestNGCucumberTests{
+public class RunCucumberTestNG extends AbstractTestNGCucumberTests {
+
+    /**
+     * Ensure the report directory exists before any tests run.
+     */
+    @BeforeClass(alwaysRun = true)
+    public static void ensureReportDirectoryExists() {
+        File reportDir = new File("target/cucumber-reports");
+        if (!reportDir.exists() && !reportDir.mkdirs()) {
+            throw new RuntimeException(
+                "Failed to create report directory: " + reportDir.getAbsolutePath()
+            );
+        }
+    }
 }
-
-
-// package StepDefinitions;
-
-// import io.cucumber.testng.AbstractTestNGCucumberTests;
-// import io.cucumber.testng.CucumberOptions;
-
-// @CucumberOptions(
-//     features = "src/test/resources/features",
-//     glue = "StepDefinitions",
-//     plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"}
-// )
-// public class RunCucumberTestNG extends AbstractTestNGCucumberTests {
-// }
 
